4d4a02187d4bbeaf4486023e79dadf31ad36da53
David Sng <davidsnghs@gmail.com>
2018-12-14 10:38:55 +0800

    Updated for PWA test

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ export class Card extends Component {
   render() {
     return (
       <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-        <img src={`https://robohash.org/${this.props.id}?200x200`} alt='' />
+        <img src={`https://robohash.org/${this.props.id}?size=200x200`} alt='' />
         <div>
             <h2>{this.props.name}</h2>
             <p>{this.props.email}</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -7,6 +7,7 @@ export class SearchBox extends Component {
     return (
       <div className="pa2 tc">
         <input
+        aria-label='Search Robot'
           type="search"
           className="pa3 ba b--green bg-lightest-blue"
           placeholder="Search Robots"
