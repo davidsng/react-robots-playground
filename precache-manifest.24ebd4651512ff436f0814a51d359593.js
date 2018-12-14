85bd1836ad0aba42564caa03fb3d9f55bcc6f084
David Sng <davidsnghs@gmail.com>
2018-12-14 09:00:26 +0800

    Updates

---
+++ precache-manifest.24ebd4651512ff436f0814a51d359593.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/davidsng/react-robots-playground/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fab6141c8f3432b4a476",
+    "url": "/davidsng/react-robots-playground/static/js/runtime~main.fab6141c.js"
+  },
+  {
+    "revision": "e4fa3fe14089076ba951",
+    "url": "/davidsng/react-robots-playground/static/js/main.e4fa3fe1.chunk.js"
+  },
+  {
+    "revision": "e244af4bac3183f495b2",
+    "url": "/davidsng/react-robots-playground/static/js/1.e244af4b.chunk.js"
+  },
+  {
+    "revision": "e4fa3fe14089076ba951",
+    "url": "/davidsng/react-robots-playground/static/css/main.e1c17e02.chunk.css"
+  },
+  {
+    "revision": "e244af4bac3183f495b2",
+    "url": "/davidsng/react-robots-playground/static/css/1.e179dae8.chunk.css"
+  },
+  {
+    "revision": "0b96b7f8b04b115e50df0d3a969af34b",
+    "url": "/davidsng/react-robots-playground/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/davidsng/react-robots-playground/precache-manifest.24ebd4651512ff436f0814a51d359593.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/davidsng/react-robots-playground/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
